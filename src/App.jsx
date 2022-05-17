70b184a00c32f4d9fbb912ab4d1e8baec77f966e
gopimudumal99 <gopimudumal99@gmail.com>
2022-05-17 17:44:17 +0530

    adding a frontend part

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { Map,Marker, GoogleApiWrapper } from "google-maps-react";
+import data from "../data.json";
+function App(props) {
+  return (
+    <div className="App">
+      <Map
+        google={props.google}
+        style={{ width: "100%", height: "100%", position: "relative" }}
+        className={"map"}
+        zoom={5}
+      >
+        {data.map((el, i) => {
+          return (
+            <Marker key={el.id} name={el.name} position={{ lat: el.lat, lng: el.long }} />
+          );
+        })}
+      </Map>
+    </div>
+  )
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDP_PFyWyBQC_RIi-9LOX8qQl_puNyaAsI"
+})(App); 
